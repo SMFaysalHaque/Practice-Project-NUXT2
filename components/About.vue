1df56017b2ded86e67e4e384e9a251fd7d449cbc
SMFaysalHaque <smfaysalhaque007@gmail.com>
2023-06-22 19:01:39 +0600

    routing clear of nuxt.js

---
+++ components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>About</h3>
+        <NuxtLink to="/"><button class="border border-s-teal-200 bg-sky-300">Go To Home</button></NuxtLink> 
+        <!-- here "/" is index.vue -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <NuxtLink to="/about"><button class="border border-s-teal-200 bg-sky-300">Go To About</button></NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style scoped>
+
+</style>
